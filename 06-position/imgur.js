b18001f233c3a55de53bec8fc85778b6eef7c375
Jason Madar <jason@vandrico.com>
2017-03-01 17:24:12 -0800

    position and transition example

---
+++ 06-position/imgur.js
@@ -0,0 +1,78 @@
+
+// First import jquery
+
+window.addEventListener('load', function() {
+    
+    var body = document.querySelector("body");
+    var jqueryScriptNode = document.createElement("script");
+    jqueryScriptNode.src = "https://code.jquery.com/jquery-3.1.1.min.js";
+    body.appendChild(jqueryScriptNode);
+
+    var categories = document.querySelectorAll("li");
+    console.log(categories);
+    categories.forEach(function(item) {
+        item.addEventListener('click', function() {
+            console.log("Switching category to "+item.innerHTML);
+            fetchImgur('https://api.imgur.com/3/gallery/r/'+item.innerHTML+'/top/0.json');
+        });
+    });
+    
+    jqueryScriptNode.addEventListener('load', function() {
+        fetchImgur('https://api.imgur.com/3/gallery/r/aww/top/0.json');
+    });    
+    
+});
+
+
+function fetchImgur(url) {
+    var numImages = $("#album").attr("num-images");
+    var imageSize = $("#album").attr("image-size") || 's';        
+    
+    console.log(numImages, imageSize);
+    
+    $.ajax(url,
+           {
+               headers: {
+                   Authorization: 'Client-ID c14a24f452ea28e'                         
+               },
+               success: function(res) {
+                   console.log(res);
+                   images = res.data;                
+                   populateImage(images, numImages, imageSize);
+               }   
+           }
+    );        
+}
+
+function populateImage(images, numImages, imageSize) {
+
+    // first clear all images in #album
+    $("#album").html("");
+    
+    var populated = 0;    
+    images.filter(function(img) {
+        return !img.is_album
+            && !img.animated
+            && ["image/png", "image/gif", "image/jpeg"].includes(img.type);
+    }).forEach(function(img) {
+        
+        if (numImages && populated++ >= numImages) return;
+        
+        var imgDiv = document.createElement("div");                
+        var imgNode = document.createElement("img");
+        imgNode.src = img.link.substr(0, img.link.lastIndexOf('.'))
+                    + imageSize + img.link.substr(img.link.lastIndexOf('.'));                
+        var textNode = document.createElement("p");
+        textNode.innerHTML = img.title;
+        
+        imgDiv.appendChild(imgNode);
+        imgDiv.appendChild(textNode);                
+        $("#album").append(imgDiv);
+        //$("#album").append(imgNode);
+        //$("#album").append(textNode);
+        
+    });
+}
+
+
+
