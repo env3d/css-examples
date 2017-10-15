c134e784bce19dc3c5e7646dcc619c53a68b4c20
Jason Madar <jason@vandrico.com>
2017-10-14 17:27:34 -0700

    added videos to the project readme, also updated server functions to send random messages

--- project-functions/functions/index.js
+++ project-functions/functions/index.js
@@ -10,12 +10,30 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+const messages = [
+    'Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.',
+    'Ut labores minimum atomorum pro. Laudem tibique ut has.',
+    'Fugit adolescens vis et, ei graeci forensibus sed.',
+    'Convenire definiebas scriptorem eu cum. Sit dolor dicunt consectetuer no.',
+    'Ea duis bonorum nec, falli paulo aliquid ei eum.',
+    'Usu eu novum principes, vel quodsi aliquip ea.',
+    'Has at minim mucius aliquam, est id tempor laoreet.',
+    'Pro saepe pertinax ei, ad pri animal labores suscipiantur.',
+    'Detracto suavitate repudiandae no eum. Id adhuc minim soluta nam.',
+    'Iisque perfecto dissentiet cum et, sit ut quot mandamus, ut vim tibique splendide instructior.',
+    'Id nam odio natum malorum, tibique copiosae expetenda mel ea.',
+    'Cu mei vide viris gloriatur, at populo eripuit sit.',
+    'Modus commodo minimum eum te, vero utinam assueverit per eu.',
+    'No nam ipsum lorem aliquip, accumsan quaerendum ei usu.'
+];
+
 
 exports.addMessage = functions.https.onRequest( (req, res) => {
 
+    randIndex = Math.floor(Math.random() * messages.length);
     admin.database().ref('rooms/lobby/messages').push({
         connectionId: 'system',
-        message: 'random message from system',
+        message: messages[randIndex],
         time: (new Date()).toUTCString(),
         user: 'system'
     });
--- project/README.md
+++ project/README.md
@@ -8,6 +8,20 @@ Nothing is kept on the server and once a page is refreshed, what is
 spoken in the chat room cannot be recovered.  That's why we called this
 app "Water Cooler Chat" -- just a casual conversation.
 
+Your solution to exercises 1 - 9 should look like the following video:
+<a href="http://www.youtube.com/watch?feature=player_embedded&v=BV5yZaX4Ae4" 
+target="_blank"><img src="http://img.youtube.com/vi/BV5yZaX4Ae4/0.jpg" 
+alt="IMAGE ALT TEXT HERE" width="240" height="180" border="10" /></a>
+
+After completing exercises 1 - 9, try to style your chat application to
+look like popular IM apps such as Slack, WhatsApp, or Google Hangout.  Here
+is my attempt at a responsive styled version:
+<a href="http://www.youtube.com/watch?feature=player_embedded&v=0HcS57iR3Zg" 
+target="_blank"><img src="http://img.youtube.com/vi/0HcS57iR3Zg/0.jpg" 
+alt="IMAGE ALT TEXT HERE" width="240" height="180" border="10" /></a>
+
+
+
 # App structure
 
 The chat app UI is described using HTML, and has several parameters
